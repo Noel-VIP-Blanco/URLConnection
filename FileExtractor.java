4baf39a3d7c6d8cc62f349fa08252035354ec145
unknown <noel.vip.blanco@gmail.com>
2023-05-06 12:42:40 +0800

    FINAL SOLUTION

--- FileExtractor.java
+++ FileExtractor.java
@@ -2,6 +2,11 @@ import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.net.*; 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -14,7 +19,8 @@ public class FileExtractor implements ActionListener
     private static JTextArea contentsTextArea;
     private static JPanel panel;
     private static JScrollPane scroll;
-    private static JButton loadButton;
+    private static JButton loadButton, exitButton;
+    private static String wholeContent = "";
     public static void main(String[] args){
         frame=new JFrame();
         frame.setSize(1000, 700);
@@ -45,31 +51,51 @@ public class FileExtractor implements ActionListener
         loadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                
                 try{
-                    URL url = new URL("https://gutenberg.org/cache/epub/70700/pg70700.txt");
+                    String urlPath = urlTextField.getText();
+                    if(urlPath.isEmpty()){
+                    JOptionPane.showMessageDialog(null,"URL IS EMPTY","Error",JOptionPane.WARNING_MESSAGE);
+                    return;
+                    }
+                    URL url = new URL(urlPath);
                     URLConnection connection = url.openConnection();
 
                     // Read the contents of the URL
-                    //BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                     String line;
-                } catch (IOException ex) {
+                    while ((line = reader.readLine()) != null) {
+                        wholeContent += line + "\n";
+                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null,"URL IS INVALID","Error",JOptionPane.WARNING_MESSAGE);
                     ex.printStackTrace();
-            }   
+                }   
                 
+                contentsTextArea.setText(wholeContent);
             }
         });
         panel.add(loadButton);
         
-        
-        
         contentsTextArea = new JTextArea();
-        contentsTextArea.setBounds(200, 100, 600, 800);
+        contentsTextArea.setBounds(200, 100, 600, 500);
         contentsTextArea.setEditable(false);
-        panel.add(contentsTextArea);
+        //panel.add(contentsTextArea);
         
-        //scroll = new JScrollPane(contentsTextArea);
-        //scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        //panel.add(scroll);
+        scroll = new JScrollPane(contentsTextArea);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.setBounds(200, 100, 600, 500);
+        panel.add(scroll);
+        
+        exitButton = new JButton("Exit");
+        exitButton.setBounds(450, 630, 100, 20);
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        panel.add(exitButton);
         
         frame.setVisible(true);
     }
